f07f00683d7327b1d349d62a4b3fb31320d3e712
gitee-hello <ninganddell@outlook.com>
2021-08-25 18:09:56 +0800

    配置了devServer,使webpack-dev-server可以自动打开浏览器

--- webpack_basic_use/webpack.config.js
+++ webpack_basic_use/webpack.config.js
@@ -6,7 +6,7 @@ const htmlWebpackPlugin = new HtmlWebPackPlugin({
     // 指定源文件的存放路径
     template: './src/index.html',
     // 指定生成的文件存放路径(在内存中生成)
-    filename: './index.html'
+    filename: 'index.html'
 })
 module.export = {
     mode:'development',
@@ -14,8 +14,17 @@ module.export = {
     entry : path.join(__dirname,'./src/index.js'),
     // 打包出口文件存放路径
     output : {
-        path: path.join(__dirname,'dist')
+        path: path.join(__dirname,'dist'),
+        publicPath: './dist'
     },
     // 把插件挂载到 webpack(使用插件),在webpack 运行的时候,会加载并且调用这些插件
-    plugins: [htmlWebpackPlugin]
+    plugins: [htmlWebpackPlugin],
+    devServer:{
+        // 初次打包完成后,自动打开浏览器
+        open:true,
+        // 实时打包所使用的主机地址
+        host:'127.0.0.1',
+        // 实时打包所使用的端口号
+        port:80
+    }
 }
