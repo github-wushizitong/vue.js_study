9848c3e96bcdeca54496c6970cd0b68053c162d8
gitee-hello <ninganddell@outlook.com>
2021-08-25 12:49:02 +0800

    回到了使用webpack的基本使用

--- webpack_basic_use/src/index.js
+++ webpack_basic_use/src/index.js
@@ -1,8 +1,5 @@
 // 1,使用ES6语法导入第三方包jQuery
 import $ from 'jquery'
-// 引入第三方包lodash
-import _ from 'lodash'
-
 // 定义jQuery的入口函数
 $(function(){
     // 实现奇偶行变色
--- webpack_basic_use/webpack.config.js
+++ webpack_basic_use/webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 module.export = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        // path.resolve() 方法将路径或路径片段的序列解析为绝对路径。
-        path: path.resolve(__dirname,'dist')
-    }
+    mode:'development',
+    // 打包入口文件路径
+    entry : path.join(__dirname,'./src/index.js'),
+    // 打包出口文件存放路径
+    output : path.join(__dirname,'./dist'),
+    // 指定输出文件的名称
+    filename : 'bundle.js'
 }
