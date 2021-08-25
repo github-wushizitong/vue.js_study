9abe6899ddb112921d54157eda376d1edf93a612
gitee-hello <ninganddell@outlook.com>
2021-08-25 20:00:58 +0800

    下载和配置了style-loader和css-loader用来打包构建css文件

--- webpack_basic_use/src/index.js
+++ webpack_basic_use/src/index.js
@@ -1,5 +1,7 @@
 // 1,使用ES6语法导入第三方包jQuery
 import $ from 'jquery'
+// 导入样式(在 webpack 中,一切皆模块,都可以通过ES6语法导入和使用)
+import './css/index.css'
 // 定义jQuery的入口函数
 $(function(){
     // 实现奇偶行变色
--- webpack_basic_use/webpack.config.js
+++ webpack_basic_use/webpack.config.js
@@ -6,7 +6,7 @@ const htmlWebpackPlugin = new HtmlWebPackPlugin({
     // 指定源文件的存放路径
     template: './src/index.html',
     // 指定生成的文件存放路径(在内存中生成)
-    filename: 'index.html'
+    filename: './index.html'
 })
 module.export = {
     mode:'development',
@@ -15,7 +15,8 @@ module.export = {
     // 打包出口文件存放路径
     output : {
         path: path.join(__dirname,'dist'),
-        publicPath: './dist'
+        // 生成的文件名称
+        filename:'bundle.js'
     },
     // 把插件挂载到 webpack(使用插件),在webpack 运行的时候,会加载并且调用这些插件
     plugins: [htmlWebpackPlugin],
@@ -26,5 +27,14 @@ module.export = {
         host:'127.0.0.1',
         // 实时打包所使用的端口号
         port:80
+    },
+    module: {
+        rules: [
+            // 定义了不同模块对应的 loader 
+            {
+                test: /\.css$/,
+                use:['style-loader','css-loader']
+            }
+        ]
     }
 }
