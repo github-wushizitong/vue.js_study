c9e378d2aabd1dbb0da90cfc910e101929baeeb9
gitee-hello <ninganddell@outlook.com>
2021-08-25 13:34:06 +0800

    基本了解了webpack-dev-server插件的使用

--- webpack_basic_use/src/index.js
+++ webpack_basic_use/src/index.js
@@ -3,6 +3,6 @@ import $ from 'jquery'
 // 定义jQuery的入口函数
 $(function(){
     // 实现奇偶行变色
-    $('li:odd').css('background-color','yellow')
-    $('li:even').css('background-color','green')
+    $('li:odd').css('background-color','blue')
+    $('li:even').css('background-color','yellow')
 })
--- webpack_basic_use/webpack.config.js
+++ webpack_basic_use/webpack.config.js
@@ -4,7 +4,7 @@ module.export = {
     // 打包入口文件路径
     entry : path.join(__dirname,'./src/index.js'),
     // 打包出口文件存放路径
-    output : path.join(__dirname,'./dist'),
-    // 指定输出文件的名称
-    filename : 'bundle.js'
-}
+    output : {
+        path: path.join(__dirname,'dist')
+    }
+    }
