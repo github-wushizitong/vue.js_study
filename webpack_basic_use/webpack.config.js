ccf318b95ce9cc1e0a50f8b56b1d7bc4499c3ccf
gitee-hello <ninganddell@outlook.com>
2021-08-26 10:18:50 +0800

    配置了webpack的打包发布和source map和源代码报错的行数一致的使用

--- webpack_basic_use/webpack.config.js
+++ webpack_basic_use/webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 // 引入 webpack 的 HTML 插件,得到一个构造函数
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+//  引入 webpack 的清理 dist 目录旧文件插件
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 // 创建 插件的实例对象
 const htmlWebpackPlugin = new HtmlWebPackPlugin({
     // 指定源文件的存放路径
@@ -8,7 +10,10 @@ const htmlWebpackPlugin = new HtmlWebPackPlugin({
     // 指定生成的文件存放路径(在内存中生成)
     filename: './index.html'
 })
+// 创建 清理 dist 目录旧文件插件的实例对象
+const leanPlugin = new CleanWebpackPlugin();
 module.export = {
+    devtool: 'nosources-source-map',
     mode:'development',
     // 打包入口文件路径
     entry : path.join(__dirname,'./src/index.js'),
@@ -16,10 +21,10 @@ module.export = {
     output : {
         path: path.join(__dirname,'dist'),
         // 生成的文件名称
-        filename:'bundle.js'
+        filename:'js/bundle.js'
     },
     // 把插件挂载到 webpack(使用插件),在webpack 运行的时候,会加载并且调用这些插件
-    plugins: [htmlWebpackPlugin],
+    plugins: [htmlWebpackPlugin,leanPlugin],
     devServer:{
         // 初次打包完成后,自动打开浏览器
         open:true,
@@ -43,7 +48,7 @@ module.export = {
             // 如果需要调用的loader 只有一个,则只传递一个字符串,如果多个,需要用数组
             {
                 test:/\.jpg|png|gif$/,
-                use: 'url-loader'
+                use: 'url-loader?limit=22229&outputPath=images'
             },
             {
                 test: /\.js$/,
