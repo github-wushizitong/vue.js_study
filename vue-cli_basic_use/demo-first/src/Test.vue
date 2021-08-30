935c5f6c0859c260852dd268ed389470de9c7473
gitee-hello <ninganddell@outlook.com>
2021-08-30 21:05:23 +0800

    vue组件中的三个组成部分,组件中的data数据源需要默认导出并且是必须是函数

--- vue-cli_basic_use/demo-first/src/Test.vue
+++ vue-cli_basic_use/demo-first/src/Test.vue
@@ -1,5 +1,25 @@
 <template>
-    <div>
-        <h1>这事用户自定义的 Test.vue</h1>
+    <div class="test-box">
+        <h1>这事用户自定义的 Test.vue-----{{username}}</h1>
     </div>
 </template>
+<script>
+// 默认导出,这事组件的固定写法
+export default {
+    // data 数据源
+    // 注意: vue 组件中的 data 不能指向对象
+    // 注意: 组件中的 data 必须是一个函数
+    data(){
+        return{
+            username:'admin'
+        }
+    }
+}
+</script>
+<style lang="less">
+    .test-box{
+        background-color: pink;
+        width: 500px;
+        height: 500px;
+    }
+</style>
