3029ff32fa112afd1f809c39d5c746b9f3f6f7db
gitee-hello <ninganddell@outlook.com>
2021-08-31 12:39:03 +0800

    组件中的 props 数组中的数据都是只读的

--- vue-cli_basic_use/demo-first/src/components/Count.vue
+++ vue-cli_basic_use/demo-first/src/components/Count.vue
@@ -10,10 +10,12 @@
     export default {
         name:'Count',
         // 父组件通过行内自定义属性传递过来的值都是用 子组件中 props 数组中对应的参数名称接收
+        // props 数组中的数据 是只读的
+        //  不能修改 数组中的数据,否则 会报错
         props:['init'],
         data() {
             return {
-                count:0
+                count:this.init
             }
         }
         
