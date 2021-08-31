a854841001dd8aa84f0a3998c17d884493b0c348
gitee-hello <ninganddell@outlook.com>
2021-08-31 20:59:04 +0800

    通过子组件的自定义事件向父组件传递参数,父组件通过行内的自定义事件回调接收子组件传递的参数

--- vue-cli_basic_use/demo-first/src/App.vue
+++ vue-cli_basic_use/demo-first/src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="app-components">
     <h1>我是APP.uve组件</h1>
+    <h2>count值为:{{count}}</h2>
     <hr>
     <div class="box">
       <!-- 使用子组件 -->
       <Left :msg="message" :user="userinfo"></Left>
+      <Right @numChange="getRightComponentNumber"></Right>
     </div>
   </div>
 </template>
@@ -12,10 +14,12 @@
 <script>
   // 导入子组件
   import Left from '@/components/Left.vue'
+  import Right from '@/components/Right.vue'
   export default {
     naem:'App',
     data(){
       return {
+        count:0,
         message:'我是父组件传递过去的message',
         userinfo:{
           name:'wsc',
@@ -23,9 +27,16 @@
         }
       }
     },
+    methods: {
+      getRightComponentNumber(value){
+        // console.log(value);
+        this.count = value;
+      }
+    },
     // 挂载跟组件
     components:{
-      Left
+      Left,
+      Right
     }
   }
 </script>
@@ -34,5 +45,8 @@
     .app-components{
       padding:1px 20px 20px;
       background-color: blue;
+      .box{
+        display: flex;
+      }
     }
 </style>
---
+++ vue-cli_basic_use/demo-first/src/components/Right.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="right-components">
+        <p>{{count}}</p>
+        <button @click="add">+1</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                count:0
+            }
+        },
+        methods: {
+            add(){
+                this.count += 1;
+                // 通过自定义事件向父组件传递参数
+                this.$emit('numChange',this.count);
+            }
+        },
+    }
+</script>
+
+<style lang="less" scoped>
+    .right-components{
+        padding:0 20px 20px;
+        background-color:slateblue;
+        flex:1;
+    }
+</style>
