cb5e9a0333ed09f9b10f0d7e51d53ca8a919136b
gitee-hello <ninganddell@outlook.com>
2021-09-02 12:43:02 +0800

    项目中安装axios 并且发送请求返回数据

--- vue-cli_shoppingCart_demo/demo-shopping_cart/src/App.vue
+++ vue-cli_shoppingCart_demo/demo-shopping_cart/src/App.vue
@@ -8,17 +8,34 @@
 </template>
 
 <script>
+  // 导入 axios 请求框架
+  import axios from 'axios'
   // 导入 Header.vue 子组件
   import Header from '@/components/Header/Header.vue'
+
   export default {
     data(){
       return {
         
       }
     },
+    // 组件中使用的方法
+    methods:{
+      async initCartList(){
+        // 调用 axios 的get 方法,请求列表数据,解构出来的 data对象重新命名为 res
+        const {data:res} = await axios.get('https://www.escook.cn/api/cart')
+        console.log(res);
+      }
+    },
+    // 挂载子组件的components节点
     components:{
       'Header':Header
+    },
+    // 生命周期函数 
+    created(){
+      this.initCartList();
     }
+
   }
 </script>
 
