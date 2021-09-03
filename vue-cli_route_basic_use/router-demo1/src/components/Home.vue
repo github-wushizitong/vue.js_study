926aa186efb82b081d8a2c4fccfaac25fc230d2c
gitee-hello <ninganddell@outlook.com>
2021-09-04 00:00:06 +0800

    声明式导航,a标签是和router-link标签都是声明式导航,使用导航方法就是编程时导航

--- vue-cli_route_basic_use/router-demo1/src/components/Home.vue
+++ vue-cli_route_basic_use/router-demo1/src/components/Home.vue
@@ -1,12 +1,30 @@
 <template>
   <div class="home-container">
     <h3>Home 组件</h3>
+    <hr />
+    <button @click="goto_dianying1">跳转到 电影1页面</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home'
+  name: "Home",
+  methods: {
+    goto_dianying1() {
+      // 编程时导航API
+      // console.log(this)
+      // 跳转到指定的hash地址,并增加一条历史记录
+      // this.$router.push("/movie/1")
+      // 跳转到指定的 hash 地址,并替换掉当前的历史记录
+      // this.$replace("/movie/1")
+      // 页面的前进和后退方法,正数代表前进,负数代表后退,如果后退从层数超过上限,则原地不动
+      // this.$router.go(-1)
+      // 在历史记录中,后退到上个页面
+      // this.$router.back()
+      // 历史记录中,前进到下一个页面
+      this.forward()
+    }
+  }
 }
 </script>
 
