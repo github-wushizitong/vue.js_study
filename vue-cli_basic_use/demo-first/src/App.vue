021acf6c4de28705c643430a6c84903f164993cf
gitee-hello <ninganddell@outlook.com>
2021-09-03 10:35:58 +0800

    App.vue中使用了keep-alive标签包裹动态组件component标签,使动态组件被缓存在内存中不被销毁,并且使用了keep-alive标签的生命周期函数 deactivated和activated

--- vue-cli_basic_use/demo-first/src/App.vue
+++ vue-cli_basic_use/demo-first/src/App.vue
@@ -4,9 +4,17 @@
     <button @click="comName = 'Left' ">展示Left</button>
     <button @click="comName = 'Right' ">展示Right</button>
     <hr>
-    <!-- 使用 vue.js  内置组件占位符 渲染组件 -->
-    <!-- is 属性值 表示要渲染的子组件的名字 -->
-    <component :is="comName"></component>
+    <!-- keep-alive   vue.js内置标签,保持Left组件隐藏或者切换的时候不被销毁,保存状态(组件被缓存到内存中了) -->
+    <!-- keep-alive 对应的生命周期函数:
+            当组件'被缓存'时,会自动触发组件的'deactivated'生命周期函数
+            当组件'被激活'时,会自动触发组件的'activated'生命周期函数
+     -->
+    <keep-alive>
+      <!-- 使用 vue.js  内置组件占位符 渲染组件 -->
+      <!-- is 属性值 表示要渲染的子组件的名字 -->
+      <component :is="comName"></component>
+    
+    </keep-alive>
 
   </div>
 </template>
--- vue-cli_basic_use/demo-first/src/components/Left.vue
+++ vue-cli_basic_use/demo-first/src/components/Left.vue
@@ -14,6 +14,22 @@
         },
         methods: {
         },
+        created(){
+            console.log('Left 组件被创建了');
+        },
+        destroyed(){
+            console.log('Left 组件被销毁了~~~~');
+        },
+        // keep-alive 对应的生命周期函数:
+        //         当组件'被缓存'时,会自动触发组件的'deactivated'生命周期函数
+        //         当组件'被激活'时,会自动触发组件的'activated'生命周期函数
+        
+        deactivated(){
+            console.log('子组件被缓存');
+        },
+        activated(){
+            console.log('子组件被激活');
+        }
     }
 </script>
 
