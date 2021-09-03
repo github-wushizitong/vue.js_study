f56e69a32b4eef8d39a8828aaa649fac4bde49e7
gitee-hello <ninganddell@outlook.com>
2021-09-03 15:39:01 +0800

    私有自定义指令 生命周期函数 bind和update

--- vue-cli_shoppingCart_demo/demo-shopping_cart/src/App.vue
+++ vue-cli_shoppingCart_demo/demo-shopping_cart/src/App.vue
@@ -2,6 +2,8 @@
   <div class="app-container">
     <h1 v-color>App 跟组件</h1>
     <h2 v-color="color">App 私有自定义指令 </h2>
+
+    <button @click="change_h2_text_color">改变h2标题文字颜色</button>
     <hr>
     <!-- Header 头部区域 -->
     <Header title="购物车案例"></Header>
@@ -94,6 +96,10 @@ export default {
     getNewNum(item,event){
       // console.log(event);
       item.goods_count = event;
+    },
+    // 点击按钮 改变 h2 文本颜色
+    change_h2_text_color(){
+      this.color = "yellow"
     }
   },
   components: {
@@ -106,12 +112,19 @@ export default {
   directives:{
     // 定义名为 v-color 的自定义指令,指向一个配置对象
     color:{
-      // 当指令第一次被绑定到元素上的时候,会立即触发 下面的 bind 函数
+      // bind 生命周期函数钩子：只调用一次，指令第一次绑定到元素时调用。在这里可以进行一次性的初始化设置。
+      // 当指令第一次被绑定到元素上的时候,会立即触发 下面的 自定义指令生命周期 bind 函数
       // 形参中的el表示当前指令所绑定到的 DMO 对象
       bind(el,binding){
         console.log(el,binding);
         // 默认颜色
         el.style.color = 'blue';
+        // 当元素使用 自定义指令时,传入参数,参数就是 自定义指令渲染的颜色
+        el.style.color = binding.value;
+      },
+      update(el,binding){
+        // 默认颜色
+        el.style.color = 'greenYellow';
         // 当元素使用 自定义指令时,传入参数,参数就是 自定义指令渲染的颜色
         el.style.color = binding.value;
       }
